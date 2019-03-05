24781b304727d12920529e45c2bdff6552a46232
brian desktop <cimminob@gmail.com>
2019-03-05 12:57:28 -0500

    add validation

--- app.js
+++ app.js
@@ -23,6 +23,27 @@ UI.prototype.addBookToList=function (book){
     list.appendChild(row);
 }
 
+// Show Alert
+UI.prototype.showAlert=function(msg, className){
+    //create div
+    const div=document.createElement('div');
+    //Add classes
+    div.className=`alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(msg));
+    //Get parent
+    const container=document.querySelector('.container');
+    //Get form
+    const form=document.querySelector('#book-form');
+    //Insert Alert
+    container.insertBefore(div, form);
+
+    //dissappear after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    },3000);
+}
+
 //Clear Fields
 UI.prototype.clearFields=function() {
     document.getElementById('title').value='';
@@ -44,12 +65,19 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     //instantiate UI
     const ui = new UI();
 
+    //validate UI
+    if(title === ''||author===''||isbn===''){
+        //error alert
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+
+    }
+
     //Add book to list
     ui.addBookToList(book);
 
     //clear fields
     ui.clearFields();
-
 
     e.preventDefault();
 })
